1500951738a1dfe50f053ff5cd935e598890a365
ClementLSW <clementlsw@gmail.com>
2020-02-01 10:45:44 +0800

    Animation thingamajik

--- Assets/CharacterBackLight.cs
+++ Assets/CharacterBackLight.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CharacterBackLight : MonoBehaviour
 {
+
+    [SerializeField] private GameObject flame;
+    [SerializeField][Range(0,5)] private float lightDT;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ActivateBacklight()
+    {
+        Debug.Log("ping");
+        flame.SetActive(true);
+        Invoke("DeactivateBackLight", lightDT);
+    }
+
+    private void DeactivateBackLight()
+    {
+        Debug.Log("off");
+        flame.SetActive(false);
     }
 }
